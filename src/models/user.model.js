d85385a0d7225fc0706f24492c97842a6783f8f1
Ansh3102 <anshulsharma2002.as@gmail.com>
2025-04-19 02:29:28 +0530

    JWT concept basics  and models updated, pre middleware and methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import { Schema } from "mongoose";
 import mongoose  from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"; 
 
 
 const userSchema = new Schema({
@@ -50,5 +52,40 @@ const userSchema = new Schema({
 {
     timestamps: true,
 }) 
+userSchema.pre("save", async function(next) { //Here we havent use arrwo function because it will THIS will not use in Arrow function to have the context we have used normal function 
+    if(!this.isModified("password")) return next(); 
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password); 
+}
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email : this.email, 
+            userName : this.userName, 
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET, 
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET, 
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User", userSchema)
